45ad0414fb13a43d65bf72d5ded35515f0af959e
Christopher Muturi <Muturi@kth.se>
2018-04-30 20:50:19 -0400

    First Commit

---
+++ Employee_Schedule/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_Schedule.Startup))]
+namespace Employee_Schedule
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
